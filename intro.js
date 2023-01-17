29e97da7a914b32ef4c74cd553f7619dd70c20dc
ReneFuentes26 <87821722+ReneFuentes26@users.noreply.github.com>
2023-01-17 14:34:22 -0600

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,87 @@
+var nombres =['jose','maria','pedro']
+
+nombres.push('Rosa')
+4
+nombres.pop()
+'Rosa'
+nombres.unshift('Emilia')
+4
+nombres.shift
+
+nombres.shift()
+'Emilia'
+nombres.indexOf("Pedro")
+-1
+nombres.indexOf("pedro")
+2
+function funciondeclarativa(nombres){
+    console.log(`Buenas tardes, ${nombres}`)
+}
+const funcionExpresion = function(nombres){
+    console.log(`Hola, funcion expresion, ${nombres}`)}
+const funcionFlecha = nombres => { console.log(`Hola, funcion de flecha, ${nombres}`)}
+
+for(var i=0; i<nombres.length; i++){
+    funciondeclarativa(nombres[i])
+    funcionExpresion(nombres[i])
+    funcionFlecha(nombres[i])
+}
+
+///////////////////
+var data =[
+    {nombres: 'jose',edad:'25'},
+    {nombres: 'alfredo',edad:'30'},
+    {nombres: 'juan',edad:'20'},
+    ]
+
+data.forEach(function(dato){
+    console.log(dato.nombre)
+})
+3VM2772:8 undefined
+undefined
+data.filter(function(dato){
+    console.log(dato.edad>25)
+})
+VM2964:2 false
+VM2964:2 true
+VM2964:2 false
+[]
+var resultado =data.filter(function(dato){return dato.edad > 25) 
+})
+VM3115:1 Uncaught SyntaxError: Unexpected token ')'
+var resultado =data.filter(function(dato){return dato.edad > 25
+})
+undefined
+var resultado = data.map(function(dato){
+    return dato.nombre
+})
+undefined
+resultado
+(3) [undefined, undefined, undefined]
+
+/////////////////////
+var alumno = {    
+    nombre: 'Pedro',
+    apellido: 'Guardado',
+
+    mostrarDatos: Function(){
+        console.log(`${this.nombre},${this.apellido}`)}
+//////////////////////////////
+function Alumno(nombreA, edadA){
+    this.nombre = nombreA
+    this.edad = edadA
+}
+
+var alumnos = []
+
+for(let i=0; i<2;i++){
+    var nombre = prompt("Escriba el nombre")
+    var edad = prompt("Escriba la edad")
+    alumnos.push(new Alumno(nombre,edad))
+}
+2
+for(let j= i=0; i<alumnos.length;i++){
+console.log(alumnos[i])
+}
+VM933:2 Alumno {nombre: 'Jose', edad: '25'}
+VM933:2 Alumno {nombre: 'Andrea', edad: '25'}
